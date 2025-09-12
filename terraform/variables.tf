333cfb47e72630f6a97fbe5ac258cdc85c9bf16c
Rahul Nagaraju <rahulnagaraju724@gmail.com>
2025-09-12 15:55:00 -0500

    add basic resource group creation, add backend

---
+++ terraform/variables.tf
@@ -0,0 +1,15 @@
+variable "resource_group_name" {
+  description = "The name of the resource group"
+  type        = string
+}
+
+variable "location" {
+  description = "The Azure location where the resource group will be created"
+  type        = string
+}
+
+variable "tags" {
+  description = "A map of tags to assign"
+  type        = map(string)
+  default     = {}
+}
